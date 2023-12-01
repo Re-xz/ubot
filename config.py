ab50e9b8843db7d17ed6c345958ac6d69cebea3c
Re-xz only <124796779+Re-xz@users.noreply.github.com>
2023-12-02 03:40:06 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,24 +7,24 @@ from distutils.util import strtobool
 load_dotenv(".env")
 
 
-API_ID = int(getenv("API_ID", "12857763")) #optional
-API_HASH = getenv("API_HASH", "7b71e8bca0d5e1c6d8383ae818d9ec8d") #optional
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1546776438").split()))
+API_ID = int(getenv("API_ID", "29486311")) #optional
+API_HASH = getenv("API_HASH", "ffdc688dc4eee8d2585cb24155188432") #optional
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1707380693").split()))
 DEEP_AI = getenv("DEEP_AI", "d7394561-0528-4714-a1ee-edd7020b48e1")
-OWNER_ID = int(getenv("OWNER_ID", "1546776438") or 0)
-ADMIN1_ID = list(map(int, getenv("ADMIN1_ID", "1345594412").split()))
+OWNER_ID = int(getenv("OWNER_ID", "6124944836") or 0)
+ADMIN1_ID = list(map(int, getenv("ADMIN1_ID", "6124944836").split()))
 ADMIN2_ID = list(map(int, getenv("ADMIN2_ID", "0").split()))
 ADMIN3_ID = list(map(int, getenv("ADMIN2_ID", "0").split()))
 ADMIN4_ID = list(map(int, getenv("ADMIN2_ID", "0").split()))
 
 
-ADMIN1_ID.append(1345594412)
+ADMIN1_ID.append(6124944836)
 ADMIN2_ID.append(0)
 ADMIN3_ID.append(0)
 ADMIN4_ID.append(0)
 
 MONGO_URL = getenv("MONGO_URL", "mongodb+srv://fadhil01:fadhil123@fadhil01.s6lkqsq.mongodb.net/?retryWrites=true&w=majority")
-BOT_TOKEN = getenv("BOT_TOKEN", "6661117858:AAFc3SDaUp9Y5g8xjhiXI0PBAOQn8opShcY")
+BOT_TOKEN = getenv("BOT_TOKEN", "6708246076:AAHJniFClupiULkfWxpQcUeJCN9lshZUTYc")
 BOT_WORKERS = int(getenv("BOT_WORKERS", "2"))
 USER_WORKERS = int(getenv("BOT_WORKERS", "4"))
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
@@ -37,12 +37,12 @@ BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 GIT_TOKEN = getenv("GIT_TOKEN", "SHA256:azvkVegsZyKoLxKq30eyBdPZ1+iURKf/rurx70H/nZ4") #personal access token
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
-BRANCH = getenv("BRANCH", "arab") #don't change
-REPO_URL = getenv("REPO_URL", "https://github.com/fadhilabdat04/ProjectArabUbot01")
+BRANCH = getenv("BRANCH", "reon") #don't change
+REPO_URL = getenv("REPO_URL", "https://github.com/Re-xz/ubot")
 CMD_HNDLR = getenv("CMD_HNDLR", ".")
 SUPPORT = int(getenv("SUPPORT", "-1001803314750"))
 CHANNEL = int(getenv("CHANNEL", "-1001681544718"))
-SESSION1 = getenv("SESSION1", "BQFwcc4AgGW8KjHQ3pZW7hId3Zciju68pACaoU1j33015GlomTMxYxMAoVP_FCRXwmT_teOoPPK4mskEiguA-8-dBun25tUnnTEt1ob_SPKG-H6ObGun_4GSlNcy2yndiNuXoiANYjLjPqWNJzRWya4NeWbQsyr0fB1f83dvlZ6RepZzMEMbq3KkJCtZHV2_oKVpM72SsxX2hgn081hdDANe9-K1zQE2YgKxmXz5zfdYyez2K4fPutEW2eZB1yYj1hvsauhnDAXQR4_N_LGig_9zCAW2DfgnZbKyXuf94CBqsoTHAyiBPoPM1D8oudF0Anz7FMQPXWslxqS2NU3WdGwGR-A7KAAAAABcMe92AA")
+SESSION1 = getenv("SESSION1", "")
 SESSION2 = getenv("SESSION2", "")
 SESSION3 = getenv("SESSION3", "")
 SESSION4 = getenv("SESSION4", "")
